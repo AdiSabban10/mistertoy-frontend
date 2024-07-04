d0ab51f619ad7a896b228c2365b74b2cc480e255
AdiSabban10 <adialon050@gmail.com>
2024-07-04 22:53:29 +0300

    Add store & service

--- mistertoy-frontend/src/root-cmp.jsx
+++ mistertoy-frontend/src/root-cmp.jsx
@@ -7,25 +7,28 @@ import { AppFooter } from './cmps/AppFooter.jsx'
 
 import { HomePage } from './pages/HomePage.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
+import { store } from './store/store.js'
+import { ToyIndex } from './pages/ToyIndex.jsx'
 
 
 export function App() {
-  return (
-    //   <Provider store={store}>
-          <Router>
-              <section className="app">
-                  <AppHeader />
-                  <main className='main-layout'>
-                      <Routes>
-                          <Route element={<HomePage />} path="/" />
-                          <Route element={<AboutUs />} path="/about" />
-                      </Routes>
-                  </main>
-                  <AppFooter />
-              </section>
-          </Router>
-    //   </Provider>
+    return (
+        <Provider store={store}>
+            <Router>
+                <section className="app">
+                    <AppHeader />
+                    <main className='main-layout'>
+                        <Routes>
+                            <Route element={<HomePage />} path="/" />
+                            <Route element={<AboutUs />} path="/about" />
+                            <Route element={<ToyIndex />} path="/toy" />
+                        </Routes>
+                    </main>
+                    <AppFooter />
+                </section>
+            </Router>
+        </Provider>
 
-  )
+    )
 }
 
